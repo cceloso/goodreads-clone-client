c75db45395b8aff10f5998ad2b72d57894eb8e5b
cceloso <cyceloso1@up.edu.ph>
2021-08-04 00:50:57 +0800

    Add request for protected route for testing (still unauthorized)

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,14 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserService } from '../services/user.service';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  message: string = "";
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+  ) { }
 
   ngOnInit(): void {
+    this.userService.getProtected()
+      .subscribe(val => {
+        if(val) {
+          console.log(val);
+          this.message = "You are authenticated!";
+        }
+      },
+      (err) => {
+        console.log("err:", err);
+        if(err.status === 401) {
+          this.message = "You are not authorized to visit this route.";
+        }
+      },
+      () => {
+        console.log("HTTP request done");
+      });
   }
 }
